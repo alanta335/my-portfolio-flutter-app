4770c1c2d5b840613bee7c2427cc79acb9924c47
alanta335 <alanta335@gmail.com>
2021-05-09 02:27:35 +0530

    URL launch added

--- trial1/lib/main.dart
+++ trial1/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+const _url = 'tel:+91 8289463489';
+const _urlmail = 'mailto:alanta@gmail.com';
+void _launchURL() async => await launch(_url);
+void _launchmail() async => await launch(_urlmail);
 void main() => runApp(MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey[100],
@@ -65,41 +69,52 @@ void main() => runApp(MaterialApp(
               Card(
                 margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
                 color: Colors.grey.shade400,
-                child: ListTile(
-                  onTap: () => '',
-                  enableFeedback: true,
-                  horizontalTitleGap: 40,
-                  leading: Icon(
-                    Icons.call_outlined,
-                    size: 50,
-                    color: Colors.grey.shade800,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.grey.shade300),
                   ),
-                  title: Text(
-                    '+91 8289463489',
-                    style: TextStyle(
-                      fontSize: 15,
+                  onPressed: _launchURL,
+                  child: ListTile(
+                    enableFeedback: true,
+                    horizontalTitleGap: 40,
+                    leading: Icon(
+                      Icons.call_outlined,
+                      size: 50,
                       color: Colors.grey.shade800,
+                    ),
+                    title: Text(
+                      '+91 8289463489',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey.shade800,
+                      ),
                     ),
                   ),
                 ),
               ),
               Card(
                 margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                color: Colors.grey.shade400,
-                child: ListTile(
-                  onTap: () => '',
-                  enableFeedback: true,
-                  horizontalTitleGap: 40,
-                  leading: Icon(
-                    Icons.email_rounded,
-                    size: 50,
-                    color: Colors.grey.shade800,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.grey.shade300),
                   ),
-                  title: Text(
-                    'alanta@gmail.com',
-                    style: TextStyle(
-                      fontSize: 15,
+                  onPressed: _launchmail,
+                  child: ListTile(
+                    enableFeedback: true,
+                    horizontalTitleGap: 40,
+                    leading: Icon(
+                      Icons.email_rounded,
+                      size: 50,
                       color: Colors.grey.shade800,
+                    ),
+                    title: Text(
+                      'alanta@gmail.com',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey.shade800,
+                      ),
                     ),
                   ),
                 ),
